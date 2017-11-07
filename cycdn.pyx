f51902beccce212d3af37978994698d40d17a11c
dallascard <dcard@andrew.cmu.edu>
2017-11-06 22:32:20 -0800

    Added much faster cythonized version for sparse design matrices

--- cycdn.pyx
+++ cycdn.pyx
@@ -1,14 +1,304 @@
+import sys
+from optparse import OptionParser
+
 import numpy as np
+from scipy import sparse
 from scipy.special import expit
+from sklearn.linear_model import LogisticRegression
+
+from libc.math cimport exp
+from libc.math cimport log
+from libc.math cimport abs as c_abs
+
+class CDN:
+
+    def __init__(self, C=1.0, beta=0.9, sigma=0.01, lower=None, upper=None, do_elimination=True):
+        self._C = C
+        self._beta = beta
+        self._sigma = sigma
+        self._lower = lower
+        self._upper = upper
+        self._do_elimination = do_elimination
+
+        self._w = None          # model weights
+        self._L = None          # loss
+        self._R = None          # regularization penalty 
+        self._exp_nyXw = None   # stored vector of exp(-yXw) values
+        self._probs = None      # stored vector of 1/(1+exp(-yXw)) values        
+        self._active = None     # vector of active variables
+        self._v = None          
+        self._M = 0
+
+
+    def fit(self, X, y, tol=1e-5, min_epochs=2, max_epochs=200, init_w=None, verbose=0, randomize=False):
+        """
+        Coordinate descent with Newton directions for L1-regularized logistic regression
+        :param X: n x p feature matrix
+        :param y: vector of labels in {-1, +1}
+        :param max_iter:
+        :return:
+        """
+
+        n_items, n_features = X.shape
+        assert sparse.issparse(X)
+
+        yX = X.multiply(y.reshape((n_items, 1))).tocsc()
+
+        # convert sparse matrix to a set of vectors and indices
+        yX_j_starts = np.zeros(n_features, dtype=np.int32)
+        yX_j_lengths = np.zeros(n_features, dtype=np.int32)
+        yX_rows = []
+        yX_vals = []
+        index = 0
+        for j in range(n_features):                
+            yX_j_coo = yX[:, j].tocoo()
+            yX_j_length = len(yX_j_coo.data)
+            yX_j_starts[j] = index
+            yX_j_lengths[j] = yX_j_length
+            for row, val in zip(yX_j_coo.row, yX_j_coo.data):
+                yX_rows.append(row)
+                yX_vals.append(val)
+            index += yX_j_length
+        yX_rows = np.array(yX_rows, dtype=np.int32)
+        yX_vals = np.array(yX_vals)
+
+        if init_w is None:
+            self._w = np.zeros(n_features)
+        else:
+            self._w = init_w            
+
+        self._v = np.zeros(n_features)
+        self._active = np.ones(n_features, dtype=np.int32)
+        self._M = 0
+        self._exp_nyXw = np.exp(-yX.dot(self._w))
+        self._probs = 1.0 / (1.0 + self._exp_nyXw)
+        self._R = np.sum(np.abs(self._w))
+        self._L = self._C * np.sum(np.log(1.0 + self._exp_nyXw))
+
+        order = np.array(np.arange(n_features), dtype=np.int32)
+
+        for k in range(max_epochs):
+            if randomize:
+                np.random.shuffle(order)
+
+            delta, ls_steps, L, R = sparse_update(n_items, n_features, self._C, self._beta, self._sigma, self._L, self._R, self._probs, self._exp_nyXw, self._w, self._lower, self._upper, yX_j_starts, yX_j_lengths, yX_rows, yX_vals, order, self._M, self._v, self._active)
+            self._L = L
+            self._R = R
+
+            # update the threshold for eliminating variables on the next iteration
+            if self._do_elimination and k > 0:
+                M = np.max(self._v / k)
+
+            w_sum = np.sum(np.abs(self._w))
+            if w_sum > 0:
+                rel_change = delta / w_sum
+            else:
+                rel_change = 0.0
+            if verbose > 1:
+                print("epoch %d, delta=%0.5f, rel_change=%0.5f, ls_steps=%d" % (k, delta, rel_change, ls_steps))
+            if rel_change < tol and k >= min_epochs - 1:
+                if verbose > 0:
+                    print("relative change below tolerance; stoppping after %d epochs" % k)
+                return
+
+        if verbose > 0:
+            print("Maximum epochs exceeded; stopping after %d epochs" % k)
+
+    def pred_probs(self, X):
+        n, p = X.shape
+        probs = np.zeros([n, 2])
+        prob_pos = expit(X.dot(self._w))
+        probs[:, 1] = prob_pos
+        probs[:, 0] = 1.0 - prob_pos
+        return probs
+
+    def get_w(self):
+        return self._w.copy()
+
+
+cdef sparse_update(int n_items, int n_features, double C, double beta, double sigma, double L, double R, double[:] probs, double[:] exp_nyXw, double[:] w, double lower, double upper, int[:] yX_j_starts, int[:] yX_j_lengths, int[:] yX_rows, double[:] yX_vals, int[:] order, double M, double[:] v, int[:] active):
+
+        cdef int i = 0
+        cdef int j
+        cdef double running_abs_change = 0
+        cdef int running_ls_steps = 0
+        cdef double[:] exp_nyXw_new = np.zeros(n_items)
+        cdef int ls_steps
+        cdef double change
+        cdef int is_active
+        while i < n_items:
+            exp_nyXw_new[i] = exp_nyXw[i]
+            i += 1
+
+        i = 0
+        while i < n_features:
+            j = order[i]
+            if active[j] > 0:
+                ls_steps, change, L, R, v_j, is_active = sparse_update_one_coordinate(C, beta, sigma, L, R, probs, exp_nyXw, exp_nyXw_new, w[j], lower, upper, yX_j_starts[j], yX_j_lengths[j], yX_rows, yX_vals, M)
+                if c_abs(change) > 0:
+                    w[j] += change
+                v[j] = v_j
+                active[j] = is_active
+
+                running_abs_change += np.abs(change)
+                running_ls_steps += ls_steps
+            i += 1
+
+        return running_abs_change, running_ls_steps, L, R
 
 
 
-cdef double compute_hessian_element(double C, int N, double [:] expits, double [:, ::1] X, int j):
+cdef sparse_update_one_coordinate(double C, double beta, double sigma, double L, double R, double[:] probs, double[:] exp_nyXw, double[:] exp_nyXw_new, double w_j, double lower, double upper, int yX_j_start, int yX_j_length, int[:] yX_rows, double[:] yX_vals, double M):
+
+    cdef int index
+    cdef int i
+    cdef double f_val = L + R
+    cdef double thresh
+
+    cdef double d  # base Newton step
+    cdef double a = 1.0  # my name for lambda
+    cdef double line_steps = 0
+    cdef double v_j
+    cdef int active = 1
+    cdef double base_L = L
+    cdef double base_R = R
+
+    # compute the gradient and Hessian elements
+    cdef double g = compute_grad_j(C, probs,  yX_j_start, yX_j_length, yX_rows, yX_vals)
+    cdef double h = compute_hessian_element(C, probs, yX_j_start, yX_j_length, yX_rows, yX_vals)
+
+    if M > 0:
+        # if w is 0 and the gradient is small, eliminate the variable from the active set
+        if w_j == 0 and -1 + M < g < 1 - M:
+            #print("Eliminating %d" % j)                
+            active = 0
+            v_j = 0
+            a = 0.0
+            print("Eliminating a variable")
+            return line_steps, a, L, R, v_j, active
+
+    # compute a new value for updating self._M
+    if w_j > 0:
+        v_j = c_abs(g + 1)
+    elif w_j < 0:
+        v_j = c_abs(g - 1)
+    else:
+        v_j = g - 1
+        if -1 - g > v_j:
+            v_j = -1 - g
+        if 0 > v_j:
+            v_j = 0
+
+    # do soft-thresholding
+    if g + 1.0 <= h * w_j:
+        d = -(g + 1.0) / h
+    elif g - 1.0 >= h * w_j:
+        d = -(g - 1.0) / h
+    else:
+        d = -w_j
+
+    # check upper and lower limits, and set max step accordingly
+    if w_j + d < lower:
+        diff = lower - w_j
+        a = diff / d
+    if w_j + d > upper:
+        diff = upper - w_j
+        a = diff / d
+
+    # unless we've hit a bound, use line search to find how far to move in this direction
+    if a > 0 and c_abs(d) > 0:
+        # set up the threshold for convergence
+        thresh = sigma * (g * d + c_abs(w_j + d) - c_abs(w_j))
+
+        # remove the current weight from the stored 1-norm of weights
+        base_R = base_R - c_abs(w_j)
+
+        # also remove the influence of the relevant parts of exp(-yXw)
+        i = yX_j_start
+        while i < yX_j_start + yX_j_length:
+            index = yX_rows[i]
+            base_L = base_L - C * log(1.0 + exp_nyXw[index])
+            i += 1
+
+        # do line search
+        #f_new, a, i, exp_nyXw = self._line_search(yX_j, d, self._w[j], R_minus_w_j, a, thresh)
+        line_steps = sparse_line_search(C, g, h, f_val, exp_nyXw, exp_nyXw_new, yX_j_start, yX_j_length, yX_rows, yX_vals, d, w_j, base_L, base_R, a, beta, thresh)
+        a = a * (beta ** line_steps)
+
+        w_j += a * d
+
+        # update the objective pieces        
+        base_R = base_R + c_abs(w_j + a * d)
+
+        i = yX_j_start
+        while i < yX_j_start + yX_j_length:
+            index = yX_rows[i]
+            base_L = base_L + C * log(1.0 + exp_nyXw_new[index])
+            i += 1
+            # also update the relevant values of exp(-yXw) and 1/(1+exp(-yXw))
+            exp_nyXw[index] = exp_nyXw_new[index]
+            probs[index] = 1.0 / (1.0 + exp_nyXw_new[index])
+
+    return line_steps, a * d, base_L, base_R, v_j, active
+
+
+cdef double compute_hessian_element(double C, double[:] probs, int yX_j_start, int yX_j_length, int[:] yX_rows, double[:] yX_vals):
     cdef double h = 0.0
-    cdef int i = 0
-
-    while (i < N):
-        h += expits[i] * (1.0 - expits[i]) * X[i, j] * X[i, j]
+    cdef int i = yX_j_start
+    cdef int index
+    while (i < yX_j_start + yX_j_length):
+        index = yX_rows[i]
+        h += probs[index] * (1.0 - probs[index]) * yX_vals[i] * yX_vals[i]
         i += 1
     return h * C
 
+cdef double compute_grad_j(double C, double[:] probs, int yX_j_start, int yX_j_length, int[:] yX_rows, double[:] yX_vals):
+    cdef double g = 0.0
+    cdef int i = yX_j_start
+    cdef int index
+    while (i < yX_j_start + yX_j_length):
+        index = yX_rows[i]
+        g += yX_vals[i] * (probs[index] - 1.0)
+        i += 1
+    return g * C
+
+cdef double sparse_line_search(double C, double g, double h, double f_val, double[:] exp_nyXw_orig, double[:] exp_nyXw, int yX_j_start, int yX_j_length, int[:] yX_rows, double[:] yX_vals, double d, double prev_w_j, double base_L, double base_R, double a, double beta, double thresh):
+    cdef line_steps = 0
+    cdef double orig_a = a
+    cdef double step = a * d
+    cdef double w_j = prev_w_j + step
+
+    cdef double L = base_L
+    cdef double R = base_R
+
+    cdef int index
+    cdef int i = yX_j_start
+    while i < yX_j_start + yX_j_length:
+        index = yX_rows[i]
+        exp_nyXw[index] = exp_nyXw_orig[index] * exp(-step * yX_vals[i])
+        L = L + C * log(1.0 + exp_nyXw[index])
+        i += 1
+
+    R = base_R + abs(w_j)
+    cdef double f_new = L + R
+    cdef int count = 0
+    # check for convergence (and also set an upper limit)
+    while f_new - f_val > a * thresh and count < 2000:
+        line_steps += 1        
+        a = a * beta
+        step = a * d
+        w_j = prev_w_j + step
+
+        i = yX_j_start
+        L = base_L
+        while i < yX_j_start + yX_j_length:
+            index = yX_rows[i]
+            exp_nyXw[index] = exp_nyXw_orig[index] * exp(-step * yX_vals[i])
+            L = L + C * log(1.0 + exp_nyXw[index])
+            i += 1
+
+        R = base_R + abs(w_j)
+        f_new = L + R
+        count += 1
+
+    return line_steps
--- test_cycdn.py
+++ test_cycdn.py
@@ -1,6 +1,7 @@
 from optparse import OptionParser
 
 import numpy as np
+from scipy import sparse
 from scipy.special import expit
 from sklearn.linear_model import LogisticRegression
 import cycdn
@@ -9,49 +10,101 @@ import cycdn
 def main():
     usage = "%prog"
     parser = OptionParser(usage=usage)
-    #parser.add_option('--keyword', dest='key', default=None,
-    #                  help='Keyword argument: default=%default')
-    #parser.add_option('--boolarg', action="store_true", dest="boolarg", default=False,
-    #                  help='Keyword argument: default=%default')
+    parser.add_option('--elim', action="store_true", dest="elimination", default=False,
+                      help='Do heuristic variable elimination: default=%default')
+    parser.add_option('--lower', dest='lower', default=-1000000.0,
+                      help='Lower limit for weights: default=%default')
+    parser.add_option('--upper', dest='upper', default=1000000.0,
+                      help='Upper limit for weights: default=%default')
+    parser.add_option('--tol', dest='tol', default=1e-5,
+                      help='Tolerance for convergence (relative change in objective): default=%default')
+    parser.add_option('--max_iter', dest='max_iter', default=200,
+                      help='Maximum number of iterations: default=%default')
+    parser.add_option('-n', dest='n', default=1000,
+                      help='Number of instances: default=%default')
+    parser.add_option('-p', dest='p', default=50,
+                      help='Number of features: default=%default')
+    parser.add_option('--sparsity_X', dest='sparsity_X', default=0.5,
+                      help='Expected proportion of zero entries in X: default=%default')
+    parser.add_option('--sparsity_beta', dest='sparsity_beta', default=0.5,
+                      help='Expected proportion of zero entries in beta: default=%default')
+    parser.add_option('--nonlinear', action="store_true", dest="nonlinear", default=False,
+                      help='Generate nonlinear data for testing: default=%default')
+    parser.add_option('--seed', dest='seed', default=None,
+                      help='Random seed: default=%default')
+    parser.add_option('--skl', action="store_true", dest="skl", default=False,
+                      help='Use sklearn implementation: default=%default')
+    parser.add_option('--both', action="store_true", dest="both", default=False,
+                      help='Run both implementations and compare: default=%default')
+    parser.add_option('-v', dest='verbose', default=2,
+                      help='Verbosity level: default=%default')
 
     (options, args) = parser.parse_args()
 
-    n = 1000
-    p = 6
-    np.random.seed(42)
-    X = np.array(np.random.randint(low=0, high=2, size=(n, p)), dtype=np.float64)
-    beta = np.array(np.random.randn(p), dtype=np.float64) * np.random.randint(low=0, high=2, size=p)
-    print(beta)
-    ps = expit(np.dot(X, beta))
+    do_elimination = options.elimination
+    lower = float(options.lower)
+    upper = float(options.upper)
+    tol = float(options.tol)
+    max_iter = int(options.max_iter)
+    n = int(options.n)
+    p = int(options.p)
+    sparsity_X = float(options.sparsity_X)
+    sparsity_beta = float(options.sparsity_beta)
+    nonlinear = options.nonlinear
+    seed = options.seed
+    use_skl = options.skl
+    use_both = options.both
+    verbose = int(options.verbose)
+
+    if seed is not None:
+        np.random.seed(int(seed))
+
+    #X = np.array(np.random.randint(low=0, high=2, size=(n, p)), dtype=np.float64)
+    X = np.array(np.random.binomial(p=1-sparsity_X, n=1, size=(n, p)), dtype=np.float64)
+    beta_mask = np.array(np.random.binomial(p=1-sparsity_beta, n=1, size=p), dtype=np.float64)
+    beta = np.array(np.random.randn(p), dtype=np.float64) * beta_mask
+    if verbose > 0:
+        print(beta)
+
+    # make a non-linear problem to encourage line search
+    if nonlinear:
+        X2 = X**2
+        beta2 = np.array(np.random.randn(p), dtype=np.float64) * np.random.randint(low=0, high=2, size=p)
+        ps = expit(np.dot(X, beta) + np.dot(X2, beta2))
+    else:
+        ps = expit(np.dot(X, beta))
     y = np.random.binomial(p=ps, n=1, size=n)
 
-    model = LogisticRegression(C=1.0, penalty='l1', fit_intercept=False)
-    model.fit(X, y)
-    print(model.coef_)
-    pred = model.predict(X)
-    print(np.sum(np.abs(y - pred)) / float(n))
-    #print(model.predict_proba(X))
+    X = sparse.csc_matrix(X)
 
-    y_float = np.array(y, dtype=np.float64)
-    y_float[y == 0] = -1.0
+    if use_skl or use_both:
+        model = LogisticRegression(C=1.0, penalty='l1', fit_intercept=False, solver='liblinear', tol=tol, max_iter=max_iter, verbose=verbose)
+        model.fit(X, y)
+        if verbose > 0:
+            print(model.coef_)
+        pred = model.predict(X)
+        if verbose > 0:
+            print(np.sum(np.abs(y - pred)) / float(n))
 
-    solver = cycdn.CDN(C=1.0)
-    w = np.array(model.coef_)
-    w = w.reshape((p, ))
-    solver.fit(X, y_float, randomize=True)
-    print(solver.get_w())
+    if (not use_skl) or use_both:
+        y2 = y.copy()
+        y2[y == 0] = -1
 
-    pred_probs = solver.pred_probs(X)
-    pred = np.argmax(pred_probs, axis=1)
-    print(np.sum(np.abs(y - pred)) / float(n))
+        solver = cycdn.CDN(C=1.0, lower=lower, upper=upper, do_elimination=do_elimination)
+        #solver.fit(X, y2, tol=1e-4, init_w=model.coef_[0], min_epochs=0, max_epochs=200, randomize=False, verbose=verbose)
+        solver.fit(X, y2, tol=tol, max_epochs=max_iter, randomize=True, verbose=verbose)
+        if verbose > 0:
+            print(solver.get_w())
 
+        pred_probs = solver.pred_probs(X)
+        pred = np.argmax(pred_probs, axis=1)
+        if verbose > 0:
+            print(np.sum(np.abs(y - pred)) / float(n))
 
-    #print(model.coef_)
-    #w = np.array(model.coef_)
-    #w = w.reshape((p, ))
-    #solver.w = w
-    #solver.test(X, y)
-    #print(solver.w)
+    if use_both:
+        diff = np.abs(model.coef_[0] - solver.get_w())
+        print("Maximum weight difference from skl:", np.max(diff))
+
 
 if __name__ == '__main__':
     main()
